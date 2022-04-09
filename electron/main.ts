a68fdc9ef0839e529a1321b1130c36af53f2e10e
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2022-04-09 15:09:10 +0200

    enable contextIsolation

--- electron/main.ts
+++ electron/main.ts
@@ -24,7 +24,7 @@ export default class Main {
             width: 800,
             height: 600,
             webPreferences: {
-                // nodeIntegration: true,
+                contextIsolation: true,
             },
         });
 
