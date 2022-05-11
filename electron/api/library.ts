f79639a1535e65f5dd7c822662fe504e3db55dd4
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2022-05-11 12:59:22 +0200

    refactor scanLibrary

--- electron/api/library.ts
+++ electron/api/library.ts
@@ -25,142 +25,73 @@ export default () => [
             let trackPaths: string[] = [];
             const pathIterator = walkIterator(libraryPaths.map((lp: LibraryPath) => lp.path));
             for (const path of pathIterator) {
-                const trackExists = await Track.findOne({ where: { path } });
+                const track = (await Track.findOne({ where: { path } })) ?? new Track();
                 const meta = await mm.parseFile(path);
                 const time = splitTime(meta.format.duration ?? 0);
-                if (!trackExists) {
-                    // Track does not exist, create it along with its album, genre, and artist
-                    // check for album, genre, artist if they exist
-                    let track = new Track();
-                    track.path = path;
-                    track.title = meta.common.title;
-                    track.duration = meta.format.duration ?? 0;
-                    track.seconds = time[2];
-                    track.minutes = time[1];
-                    track.hours = time[0];
-                    track.trackNumber = meta.common.track.no;
-                    track.diskNumber = meta.common.disk.no;
-                    track.exists = true;
-                    await track.save();
-                    let metaArtists = meta.common.artists;
-                    let metaAlbum = meta.common.album;
-                    let metaGenres = meta.common.genre;
-                    let artists: Artist[];
-                    if (metaArtists === undefined) {
-                        track.artists = [];
-                        artists = [];
-                    } else {
-                        artists = await Promise.all(
-                            metaArtists.map(async (artistName) => {
-                                let artist =
-                                    (await Artist.findOne({ where: { name: artistName } })) ??
-                                    Artist.create({
-                                        name: artistName,
-                                        albums: [],
-                                    });
-                                await artist.save();
-                                return artist;
-                            })
-                        );
-                        track.artists = artists;
-                    }
-                    if (metaGenres === undefined) {
-                        track.genres = [];
-                    } else if (metaGenres.length > 0) {
-                        const genres = metaGenres.map(async (genreName) => {
-                            let genre =
-                                (await Genre.findOne({ where: { name: genreName } })) ??
-                                Genre.create({
-                                    name: genreName,
-                                });
-                            await genre.save();
-                            return genre;
-                        });
-                        track.genres = await Promise.all(genres);
-                    }
-                    if (metaAlbum) {
-                        let album =
-                            (await Album.findOne({ where: { title: metaAlbum } })) ??
-                            Album.create({
-                                title: metaAlbum,
-                            });
-                        track.album = album;
-                        album.artists = [];
-                        await album.save();
-                        artists.forEach(async (artist) => {
-                            if (!album.artists.includes(artist)) {
-                                album.artists.push(artist);
-                            }
-                            await artist.save();
-                        });
-                        await album.save();
-                    }
-                    await track.save();
+                track.path = path;
+                track.title = meta.common.title;
+                track.duration = meta.format.duration ?? 0;
+                track.seconds = time[2];
+                track.minutes = time[1];
+                track.hours = time[0];
+                track.trackNumber = meta.common.track.no;
+                track.diskNumber = meta.common.disk.no;
+                track.exists = true;
+                await track.save();
+                let metaArtists = meta.common.artists;
+                let metaAlbum = meta.common.album;
+                let metaGenres = meta.common.genre;
+                let artists: Artist[];
+                if (metaArtists === undefined) {
+                    track.artists = [];
+                    artists = [];
                 } else {
-                    trackExists.title = meta.common.title;
-                    trackExists.duration = meta.format.duration ?? 0;
-                    trackExists.seconds = time[2];
-                    trackExists.minutes = time[1];
-                    trackExists.hours = time[0];
-                    trackExists.trackNumber = meta.common.track.no;
-                    trackExists.diskNumber = meta.common.disk.no;
-                    trackExists.exists = true;
-                    trackExists.save();
-                    let metaArtists = meta.common.artists;
-                    let metaAlbum = meta.common.album;
-                    let metaGenres = meta.common.genre;
-                    let artists: Artist[];
-                    if (metaArtists === undefined) {
-                        trackExists.artists = [];
-                        artists = [];
-                    } else {
-                        artists = await Promise.all(
-                            metaArtists.map(async (artistName) => {
-                                let artist =
-                                    (await Artist.findOne({ where: { name: artistName } })) ??
-                                    Artist.create({
-                                        name: artistName,
-                                        albums: [],
-                                    });
-                                await artist.save();
-                                return artist;
-                            })
-                        );
-                        trackExists.artists = artists;
-                    }
-                    if (metaGenres === undefined) {
-                        trackExists.genres = [];
-                    } else if (metaGenres.length > 0) {
-                        const genres = metaGenres.map(async (genreName) => {
-                            let genre =
-                                (await Genre.findOne({ where: { name: genreName } })) ??
-                                Genre.create({
-                                    name: genreName,
+                    artists = await Promise.all(
+                        metaArtists.map(async (artistName) => {
+                            let artist =
+                                (await Artist.findOne({ where: { name: artistName } })) ??
+                                Artist.create({
+                                    name: artistName,
+                                    albums: [],
                                 });
-                            await genre.save();
-                            return genre;
-                        });
-                        trackExists.genres = await Promise.all(genres);
-                    }
-                    if (metaAlbum) {
-                        let album =
-                            (await Album.findOne({ where: { title: metaAlbum } })) ??
-                            Album.create({
-                                title: metaAlbum,
-                            });
-                        trackExists.album = album;
-                        album.artists = [];
-                        await album.save();
-                        artists.forEach(async (artist) => {
-                            if (!album.artists.includes(artist)) {
-                                album.artists.push(artist);
-                            }
                             await artist.save();
-                        });
-                        await album.save();
-                    }
-                    await trackExists.save();
+                            return artist;
+                        })
+                    );
+                    track.artists = artists;
                 }
+                if (metaGenres === undefined) {
+                    track.genres = [];
+                } else if (metaGenres.length > 0) {
+                    const genres = metaGenres.map(async (genreName) => {
+                        let genre =
+                            (await Genre.findOne({ where: { name: genreName } })) ??
+                            Genre.create({
+                                name: genreName,
+                            });
+                        await genre.save();
+                        return genre;
+                    });
+                    track.genres = await Promise.all(genres);
+                }
+                if (metaAlbum) {
+                    let album =
+                        (await Album.findOne({ where: { title: metaAlbum } })) ??
+                        Album.create({
+                            title: metaAlbum,
+                        });
+                    track.album = album;
+                    album.artists = [];
+                    await album.save();
+                    artists.forEach(async (artist) => {
+                        if (!album.artists.includes(artist)) {
+                            album.artists.push(artist);
+                        }
+                        await artist.save();
+                    });
+                    await album.save();
+                }
+                await track.save();
                 trackPaths.push(path);
             }
             console.log("Scanning done.");
