f1e7f1df7b8eb23caf771f1ee707c62936c832d4
Blechlawine <marczinser@gmx.de>
2022-06-19 18:34:53 +0200

    manage window state with npm package

--- electron/main.ts
+++ electron/main.ts
@@ -3,58 +3,11 @@ import path from "path";
 import datasource from "./database/database";
 import { registerIpcHandlers } from "./ipc";
 import fs from "fs";
-
-let windowSettings = {
-    width: 1000,
-    height: 600,
-    maximized: false,
-    minimized: false,
-};
+import windowStateKeeper from "electron-window-state";
 export default class Main {
     static mainWindow: BrowserWindow | null;
     static application: Electron.App;
     static BrowserWindow: typeof BrowserWindow;
-
-    private static async loadLastWindowSettings(): Promise<any> {
-        const exists = fs.existsSync(path.join(Main.application.getPath("userData"), "window.json"));
-        if (!exists) {
-            await Main.saveWindowSettings()
-                .then(() => {
-                    console.log("Saved window settings");
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
-            return Promise.resolve(windowSettings);
-        } else {
-            return new Promise((resolve, reject) => {
-                fs.readFile(path.join(Main.application.getPath("userData"), "window.json"), "utf8", (err, data) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        console.log("Loaded window settings", data);
-                        resolve(JSON.parse(data));
-                    }
-                });
-            });
-        }
-    }
-
-    private static async saveWindowSettings() {
-        return new Promise((resolve, reject) => {
-            fs.writeFile(
-                path.join(Main.application.getPath("userData"), "window.json"),
-                JSON.stringify(windowSettings),
-                (err) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(true);
-                    }
-                }
-            );
-        });
-    }
 
     private static onWindowAllClosed() {
         if (process.platform !== "darwin") {
@@ -68,14 +21,18 @@ export default class Main {
     }
 
     private static async onReady() {
-        windowSettings = await Main.loadLastWindowSettings().catch((err) => {
-            console.error(err);
-        });
         const isDev = process.env.ENVIRONMENT == "development" ? true : false;
 
+        let mainWindowState = windowStateKeeper({
+            defaultWidth: 1000,
+            defaultHeight: 600,
+        });
+
         Main.mainWindow = new Main.BrowserWindow({
-            width: windowSettings.width,
-            height: windowSettings.height,
+            width: mainWindowState.width,
+            height: mainWindowState.height,
+            x: mainWindowState.x,
+            y: mainWindowState.y,
             frame: false,
             backgroundColor: "#121418",
             webPreferences: {
@@ -85,39 +42,24 @@ export default class Main {
             },
         });
 
+        mainWindowState.manage(Main.mainWindow);
+
         const win = Main.mainWindow;
         win.on("resize", () => {
-            windowSettings.width = win.getBounds().width;
-            windowSettings.height = win.getBounds().height;
-            Main.mainWindow?.webContents.send("windowResize", windowSettings.width, windowSettings.height);
+            Main.mainWindow?.webContents.send("windowResize", Main.mainWindow.getBounds().width, Main.mainWindow.getBounds().height);
         });
 
         win.on("maximize", () => {
-            windowSettings.maximized = true;
-            windowSettings.minimized = false;
             Main.mainWindow?.webContents.send("windowMaximize");
         });
 
         win.on("unmaximize", () => {
-            windowSettings.maximized = false;
             Main.mainWindow?.webContents.send("windowUnmaximize");
         });
 
         win.on("minimize", () => {
-            windowSettings.minimized = true;
-            windowSettings.maximized = false;
             Main.mainWindow?.webContents.send("windowMinimize");
         });
-
-        win.on("close", async () => {
-            await Main.saveWindowSettings();
-        });
-
-        if (windowSettings.maximized) {
-            Main.mainWindow.maximize();
-        } else if (windowSettings.minimized) {
-            Main.mainWindow.minimize();
-        }
 
         Main.mainWindow.loadURL(
             isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../dist/index.html")}`
