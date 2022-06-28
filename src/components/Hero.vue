272e486afa1135d909a17f363caf1f33e190daf6
Blechlawine <marczinser@gmx.de>
2022-06-28 21:19:05 +0200

    add back button to hero element

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="hero h-1/6 p-4 flex flex-row items-end justify-start">
+    <div class="hero h-1/6 p-4 flex flex-row items-end justify-start gap-4">
+        <IconButton @click="back">arrow_back</IconButton>
         <h1 class="headline">{{ props.title }}</h1>
     </div>
 </template>
 <script setup lang="ts">
+import IconButton from "./buttons/IconButton.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
 const props = defineProps({
     title: {
         type: String,
         required: true,
     },
 });
+
+const back = () => {
+    router.back();
+};
 </script>
