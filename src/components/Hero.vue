3ac72be66ea5f8add5d2a32c99e64916f488a0d9
Blechlawine <marczinser@gmx.de>
2022-06-28 21:13:55 +0200

    add headline styling to headline and add padding

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="hero h-1/6 flex flex-row items-end justify-start">
-        <h1>{{ props.title }}</h1>
+    <div class="hero h-1/6 p-4 flex flex-row items-end justify-start">
+        <h1 class="headline">{{ props.title }}</h1>
     </div>
 </template>
 <script setup lang="ts">
