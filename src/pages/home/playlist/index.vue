6278a7339238b418fe69739962c4ea1b5d735419
Blechlawine <marczinser@gmx.de>
2022-07-24 18:56:17 +0200

    add play buttons to playlist view

--- src/pages/home/playlist/index.vue
+++ src/pages/home/playlist/index.vue
@@ -1,19 +1,25 @@
 <template>
     <div class="playlist" v-if="playlist">
-        <Hero :title="playlist!.title"></Hero>
+        <Hero :title="playlist!.title">
+            <IconButton @click="play">play_arrow</IconButton>
+            <IconButton @click="playShuffled">shuffle</IconButton>
+        </Hero>
         <router-view></router-view>
     </div>
-    <div v-else>
-        Loading...
-    </div>
+    <div v-else>Loading...</div>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from "vue";
+import { computed, onMounted, nextTick } from "vue";
 import Hero from "../../../components/Hero.vue";
+import IconButton from "../../../components/buttons/IconButton.vue";
 import usePlaylist from "../../../stores/playlistStore";
+import usePlayer from "../../../stores/playerStore";
+import useTracks from "../../../stores/trackStore";
 
 const PlaylistStore = usePlaylist();
+const PlayerStore = usePlayer();
+const TrackStore = useTracks();
 
 const props = defineProps({
     id: {
@@ -27,4 +33,20 @@ const playlist = computed(() => PlaylistStore.getPlaylist(props.id));
 onMounted(() => {
     PlaylistStore.fetchAllPlaylists();
 });
+
+const play = () => {
+    if (playlist) {
+        PlayerStore.shuffle = false;
+        PlayerStore.queue = playlist.value!.playlistTracks.map(plt => TrackStore.getTrackById(plt.track.id)!.id);
+        nextTick(() => PlayerStore.play());
+    }
+};
+
+const playShuffled = () => {
+    if (playlist) {
+        PlayerStore.shuffle = true;
+        PlayerStore.queue = playlist.value!.playlistTracks.map(plt => TrackStore.getTrackById(plt.track.id)!.id);
+        nextTick(() => PlayerStore.play());
+    }
+};
 </script>
