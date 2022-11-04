6eef63e5bc01ada55d7583a5b76f55402b1fe853
Blechlawine <marczinser@gmx.de>
2022-11-04 21:37:16 +0100

    add log message to indicate electron starting

--- electron/electron.ts
+++ electron/electron.ts
@@ -1,5 +1,5 @@
 // This file just starts the electron app
 import { app, BrowserWindow } from "electron";
 import Main from "./main";
-
+console.log("Starting electron");
 Main.main(app, BrowserWindow);
